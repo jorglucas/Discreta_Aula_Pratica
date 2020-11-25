397d43aec716b6e999520b88c232b0f67aa03115
JhN-Te <jhonata.tenorio2526@gmail.com>
2020-11-25 18:56:04 -0300

    q4 correction

--- Aula_Pratica/q4.c
+++ Aula_Pratica/q4.c
@@ -1,40 +1,62 @@
 #include <stdio.h>
-
-int main() {
-    int mdc();
-    int mmc();
-    int a, b;
-    scanf("%d %d", &a, &b);
-    int res = mdc(a, b, 1, 2);
-    printf("MDC = %d\n", res);
-    res = mmc(a, b, 1, 2);
-    printf("MMC = %d\n", res);
-
-}
-
-int mdc(int a, int b, int res, int num){
     
-    if(num <= 9){
-        if(a == 1 || b == 1) return res;
-        else if(a % num == 0 && b % num == 0){
-            res *= num;
-            mdc(a / num, b / num, res, num);
-        }
-        else mdc(a, b, res, ++num);
-    }
-    else return res;
+void primo();
+void funcMmc();
+void funcMdc();
+
+int mmc = 1, mdc = 1;
+
+int main(){
+    int a, b, max;
+    scanf("%d %d", &a, &b);
+    if(a > b) max = a;
+    else if(b > a) max = b;
+    else max = a;
+
+    // Calcular o mmc:
+    primo(a, 2, a, b, 0);
+    printf("MMC = %d\n", mmc);
+
+    // Calcular o mdc:
+    funcMdc(a, b, 2);
+    printf("MDC = %d\n", mdc);
 }
 
-int mmc(int a, int b, int res, int num){
-    if(num <= 9){
-        if(a == 1 && b == 1) return res;
-        else if(a % num == 0 || b % num == 0){
-            if(a % num == 0) a /= num;  
-            if(b % num == 0) b /= num;
-            res *= num;
-            mmc(a, b, res, num);
+void primo(int max, int num, int a, int b, int mdcOrMmc){
+    if(num <= max){
+        int verify = 0;
+        for(int i = 2; i < num; i++){
+            if(num % i == 0){
+                verify = 1;
+                break;
+            }
         }
-        else mmc(a, b, res, ++num);
+        if(verify != 1){
+            funcMmc(max, num, a, b);
+        }
+        else primo(max, ++num, a, b, mdcOrMmc);
     }
-    else return res;
+    else{
+        return;
+    }
+}
+
+void funcMmc(int max, int num, int a, int b){
+    if(a % num == 0 || b % num == 0){
+        mmc *= num;
+        if(a % num == 0) a /= num;
+        if(b % num == 0) b /= num;
+        funcMmc(max, num, a, b);
+    }
+    else primo(max, ++num, a, b, 0);   
+}
+
+void funcMdc(int a, int b, int num){
+    if(num < 8){
+        if(a % num == 0 && b % num == 0){
+            mdc *= num;
+            funcMdc(a / num, b / num, num);
+        }
+        else funcMdc(a, b, ++num);
+    } 
 }
