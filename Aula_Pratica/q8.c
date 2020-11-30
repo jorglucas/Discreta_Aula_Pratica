1b2a31018e763b60f79b94a25b4d9222653e6942
JhN-Te <jhonata.tenorio2526@gmail.com>
2020-11-30 14:56:23 -0300

    60% q8

--- Aula_Pratica/q8.c
+++ Aula_Pratica/q8.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
 
-void coeficientes(int i, int quocientes[]){
+int mdc();
+void coeficientes();
+int quocientes[100], i = 0, s, t;
+
+int main(){
+    int a, b, m;
+    scanf("%d %d %d", &a, &b, &m);
+    printf("Para encontrar a solução de uma congruência do tipo\nax ≡ b mod m, informe a, b e m:\n");
+    
+    // I. achar o mdc(a, m);
+    int res = mdc(a, m);  
+    // Verificar se o mdc divide b;
+        if(b % res == 0){ 
+            // Temos as possíveis soluções (res).
+            while(res != 1){
+                a /= res;
+                b /= res;
+                m /= res;
+                i = 0;
+                res = mdc(a, m);
+            }
+            coeficientes(a, m);
+            printf("s = %d\nt = %d\n", s, t);
+
+
+        }
+
+
+}
+
+int mdc(int a, int m){
+    int resto = 0;
+    
+    quocientes[i] = a / m;
+    resto = a - (quocientes[i] * m);
+
+    if(resto == 0){
+        printf("mdc = %d\n", m);
+        return m;
+    }
+    else{
+        ++i;
+        mdc(m, resto);
+    }
+}
+
+void coeficientes(int a, int m){
     int coefs[2], res;
     coefs[0] = 0, coefs[1] = 1;
 
@@ -9,29 +55,30 @@ void coeficientes(int i, int quocientes[]){
         coefs[0] = coefs[1];
         coefs[1] = res;
     }
-    if( i % 2 == 0) coefs[0] *= -1;
-    else coefs[1] *= -1;
-
-    printf("%d\n", coefs[0]);
-
-}
-
-void mdc(int a, int b, int i, int quocientes[]){
-    int resto = 0;
-    
-    quocientes[i] = a / b;
-    resto = a - (quocientes[i] * b);
-
-    if(resto == 0){
-        coeficientes(i, quocientes);
+    if(a > m){
+        if(coefs[0] > coefs[1]){
+            if( i % 2 == 0) coefs[0] *= -1;
+            else coefs[1] *= -1;
+            s = coefs[1];
+            t = coefs[0];
+        } else{
+            if( i % 2 == 0) coefs[0] *= -1;
+            else coefs[1] *= -1;
+            s = coefs[0];
+            t = coefs[1];
+        }
     }
-    else mdc(b, resto, ++i, quocientes);
-}
-    
-int main(){
-    int a, b, m, quocientes[100];
-    scanf("%d %d %d", &a, &b, &m);
-    printf("Para encontrar a solução de uma congruência do tipo ax ≡ b mod m, informe a, b e m:\n");
-    printf("O inverso de %d (mod %d) = ", a, m);
-    mdc(a, m, 0, quocientes);
+    else{ // m > a
+        if(coefs[0] < coefs[1]){
+            if( i % 2 == 0) coefs[0] *= -1;
+            else coefs[1] *= -1;
+            s = coefs[1];
+            t = coefs[0];
+        } else{
+            if( i % 2 == 0) coefs[0] *= -1;
+            else coefs[1] *= -1;
+            s = coefs[0];
+            t = coefs[1];
+        }
+    }
 }
